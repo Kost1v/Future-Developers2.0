a1ef2a741bde81d16b0b810be5f8ef6c995d687a
Ostap <ostapcostiv@gmail.com>
2024-09-09 17:00:51 +0300

    test

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './js/header';
+import './js/hero';
+import './js/about-me';
+import './js/benefits';
+import './js/projects';
+import './js/faq';
+import './js/reviews';
+import './js/footer';
